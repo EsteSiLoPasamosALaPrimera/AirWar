8c99ef948ed7c43be910cafcebfe29a7157d9ed9
Jean Paul <jeanpaullm>
2017-03-16 20:13:35 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/AirWar__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AirWar__.dir/main.cpp.o"
+  "AirWar__.pdb"
+  "AirWar__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirWar__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
